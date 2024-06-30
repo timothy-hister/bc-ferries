789a6603078f45e40462f17f05203c93a119bcc5
djschoo <djschoo@protonmail.com>
2024-06-30 11:28:14 -0700

    test

--- printmessage.py
+++ printmessage.py
@@ -1,10 +1,12 @@
-# save_to_file.py
+# # save_to_file.py
+# 
+# message = "Hello, GitHub Actions!"
+# file_path = "output.txt"
+# 
+# # Open the file in write mode and save the message
+# with open(file_path, "w") as file:
+#     file.write(message)
+# 
+# print(f"Saved '{message}' to {file_path}")
 
-message = "Hello, GitHub Actions!"
-file_path = "output.txt"
-
-# Open the file in write mode and save the message
-with open(file_path, "w") as file:
-    file.write(message)
-
-print(f"Saved '{message}' to {file_path}")
+print('hello')
